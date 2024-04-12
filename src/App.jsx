c73e25d0827e22143db6c124f7cf8c025158022c
kimmiso <vmffjrm@naver.com>
2024-04-12 15:29:53 +0900

    fix:import.meta.env.MODE로 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./styles/components.scss";
-const icon3 = `/public/assets/icon/icon_plus.svg`;
-const icon4 = `/public/assets/icon/icon_arrow_left.svg`;
-const icon5 = `/public/assets/icon/icon_arrow_right.svg`;
+const icon3 = `${import.meta.env.MODE}/assets/icon/icon_plus.svg`;
+const icon4 = `${import.meta.env.MODE}/assets/icon/icon_arrow_left.svg`;
+const icon5 = `${import.meta.env.MODE}/assets/icon/icon_arrow_right.svg`;
 
 const titleStyle = {
   fontSize: "30px",
